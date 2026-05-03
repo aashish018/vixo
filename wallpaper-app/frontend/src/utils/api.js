1abd73dfd05888738b396fb3086dc4195586aa31
aashish018 <18aashishkr@gmail.com>
2026-05-03 21:35:42 +0530

    wallpaepr

--- wallpaper-app/frontend/src/utils/api.js
+++ wallpaper-app/frontend/src/utils/api.js
@@ -1,5 +1,5 @@
 import axios from 'axios';
-const API_BASE = import.meta.env.VITE_API_BASE_URL || '/api';;
+const API_BASE = 'https://zixo.onrender.com' || '/api';;
 
 const api = axios.create({
   baseURL: API_BASE,
